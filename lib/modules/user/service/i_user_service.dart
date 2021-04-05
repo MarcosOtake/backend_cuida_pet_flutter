4e0bf91f2fdd1ca789ce939e4a48a3c08b724d48
Rodrigo <rodrigorahman@gmail.com>
2021-04-05 20:24:05 -0300

    [Usuario] - refresh token

--- lib/modules/user/service/i_user_service.dart
+++ lib/modules/user/service/i_user_service.dart
@@ -1,5 +1,7 @@
 import 'package:cuidapet_api/entities/user.dart';
+import 'package:cuidapet_api/modules/user/view_models/refresh_token_view_model.dart';
 import 'package:cuidapet_api/modules/user/view_models/user_confirm_input_model.dart';
+import 'package:cuidapet_api/modules/user/view_models/user_refresh_token_input_model.dart';
 import 'package:cuidapet_api/modules/user/view_models/user_save_input_model.dart';
 
 abstract class IUserService {
@@ -7,4 +9,5 @@ abstract class IUserService {
   Future<User> loginWithEmailPassword(String email, String password, bool supplierUser);
   Future<User> loginWithSocial(String email, String avatar, String socialType, String socialKey);
   Future<String> confirmLogin(UserConfirmInputModel inputModel);
+  Future<RefreshTokenViewModel> refreshToken(UserRefreshTokenInputModel model);
 }
