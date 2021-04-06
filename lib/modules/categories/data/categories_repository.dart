577525237745e72d81c3f67287af54cd7fc0f65f
Rodrigo <rodrigorahman@gmail.com>
2021-04-06 12:54:54 -0300

    [Categorias] - construindo estrutura do novo módulo

---
+++ lib/modules/categories/data/categories_repository.dart
@@ -0,0 +1,8 @@
+import 'package:injectable/injectable.dart';
+
+import './i_categories_repository.dart';
+	
+@LazySingleton(as: ICategoriesRepository)
+class CategoriesRepository implements  ICategoriesRepository {
+
+}
