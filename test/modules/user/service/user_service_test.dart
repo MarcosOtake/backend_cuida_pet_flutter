4ce1eeebab18fabe3307e5cd37417f9b62740e7c
Rodrigo <rodrigorahman@gmail.com>
2021-04-22 17:54:20 -0300

    [Testes] - Implementando testes do metodo loginWithSocial na camada de service

--- test/modules/user/service/user_service_test.dart
+++ test/modules/user/service/user_service_test.dart
@@ -20,6 +20,7 @@ void main() {
     userRepository = MockUserRepository();
     log = MockLogger();
     userService = UserService(userRepository: userRepository, log: log);
+    registerFallbackValue<User>(User());
   });
 
   group('Group test loginWithEmailPassword', () {
@@ -43,22 +44,79 @@ void main() {
           email, password, supplierUser)).called(1);
     });
 
-    test('Should login with email and password and return UserNotfoundException', () async {
+    test(
+        'Should login with email and password and return UserNotfoundException',
+        () async {
       //Arrange
       final email = 'rodrigorahman@gmail.com';
       final password = '123123';
       final supplierUser = false;
       when(() => userRepository.loginWithEmailPassword(
-          email, password, supplierUser)).thenThrow(UserNotfoundException(message: 'Usuário não encontrado'));
+              email, password, supplierUser))
+          .thenThrow(UserNotfoundException(message: 'Usuário não encontrado'));
 
       //Act
       final call = userService.loginWithEmailPassword;
 
       //Assert
-      expect(() => call(
-          email, password, supplierUser), throwsA(isA<UserNotfoundException>()));
+      expect(() => call(email, password, supplierUser),
+          throwsA(isA<UserNotfoundException>()));
       verify(() => userRepository.loginWithEmailPassword(
           email, password, supplierUser)).called(1);
+    });
+  });
+
+  group('Group test loginWithSocial', () {
+    test('Should login social with success', () async {
+      //Arrange
+      final email = 'rodrigorahman@academiadoflutter.com.br';
+      final socialKey = '123';
+      final socialType = 'Facebook';
+
+      final userReturnLogin = User(
+          id: 1, email: email, socialKey: socialKey, registerType: socialType);
+      when(() => userRepository.loginByEmailSocialKey(
+              email, socialKey, socialType))
+          .thenAnswer((_) async => userReturnLogin);
+      //Act
+      final user =
+          await userService.loginWithSocial(email, '', socialType, socialKey);
+
+      //Assert
+      expect(user, userReturnLogin);
+      verify(() => userRepository.loginByEmailSocialKey(
+          email, socialKey, socialType)).called(1);
+    });
+
+    test('Should login social with user not found and create a new user',
+        () async {
+      //Arrange
+      final email = 'rodrigorahman@academiadoflutter.com.br';
+      final socialKey = '123';
+      final socialType = 'Facebook';
+
+      final userCreated = User(
+        id: 1,
+        email: email,
+        socialKey: socialKey,
+        registerType: socialType,
+      );
+
+      when(() => userRepository.loginByEmailSocialKey(
+              email, socialKey, socialType))
+          .thenThrow(UserNotfoundException(message: 'Usuário não encontrado'));
+
+      when(() => userRepository.createUser(any<User>()))
+          .thenAnswer((_) async => userCreated);
+      //Act
+      final user =
+          await userService.loginWithSocial(email, '', socialType, socialKey);
+
+      //Assert
+      expect(user, userCreated);
+      verify(() => userRepository.loginByEmailSocialKey(
+          email, socialKey, socialType)).called(1);
+      verify(() => userRepository.createUser(any<User>())).called(1);
     });
   });
 }
